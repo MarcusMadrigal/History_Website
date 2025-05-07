6f1dbdca792a654813daeca41534741ee5318314
MarcusMadrigal <2031721@apps.nsd.org>
2025-05-07 09:02:05 -0700

    Commit with new variables in script stuff.

--- public/js.js
+++ public/js.js
@@ -4,28 +4,41 @@
 
 // Draw Variable Thingy
 var LIST = [
-    "Hello.",
-    "I am computer.",
-    "*Checking search history...",
-    "...",
-    "...",
-    "WTF",
-    "An airstrike will arrive at your house in 3 minutes.",
-    "Run."
+    
+    [
+        "Hello.",
+        "I am computer.",
+        "*Checking search history...",
+        "...",
+        "...",
+        "...!",
+        "WTF",
+        "An airstrike will arrive at your house in 3 minutes.",
+        "Run."
+
+    ],
+    [
+        "AAAAAAAAAAAAAAAAAAAHHHHHHHHH",
+    ],
+    [
+        "HAHAHAHAHAHAHAHAHAHAHAHAHAHA",
+    ]
 ];
 
+var target = document.getElementById("hitMe");
+var stuffToShow = '';
 var draw = function(x) {
     for(
         var count = 0;
         count < x.length;
         count++
     ) {
-        alert(x[count]);
+        var nextList = x[count];
+        for(var i = 0; i < nextList.length; i++) {
+            alert( nextList[i]);
+            target.innerHTML = stuffToShow += nextList[i];
+        }
     }
 }
 
-draw(LIST); // Calls the function draw, which will alert us "I'm doing it!!!!" 
-         // and the count for as long as the count is less than x.
-         // In this case, it will alert us 9 times. Up until the alert says the count is 8.
-         // When the count reaches 8, it stops.
-         // This is because the count has to be less than x, and when count = 9, it is not less than x, which is 9.
+draw(LIST);
